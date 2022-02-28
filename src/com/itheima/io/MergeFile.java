31cb25edb10078b80b778d12e0c57354ac17ad24
mdhUser <734570993@qq.com>
2022-02-28 17:44:41 +0800

    IO流加强 2022/2/28

--- src/com/itheima/io/MergeFile.java
+++ src/com/itheima/io/MergeFile.java
@@ -1,7 +1,6 @@
 package com.itheima.io;
 
 import java.io.*;
-import java.util.Arrays;
 
 /**
  * @description:
@@ -9,11 +8,20 @@ import java.util.Arrays;
  * @email: maodihui@foxmail.com
  * @date: 2022/2/22 16:29
  */
-public class AddFile {
+public class MergeFile {
 
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        try {
+            merge();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void merge() throws IOException {
         int count = 0;
         while (true) {
             File file = new File("D:\\heimaHomeWork\\FeiQ.exe-" + count);
@@ -28,33 +36,21 @@ public class AddFile {
         }
 
         File file = new File("d:/FeiQ.exe");
-        byte[] target = null;
         OutputStream fos = new FileOutputStream(file);
 
         for (int i = 0; i < files.length; i++) {
             FileInputStream fis = new FileInputStream(files[i]);
             try (fis) {
                 byte[] readContent = new byte[(int) files[i].length()];
-                fis.read(readContent);
-                if (i == 0)
-                    target = Arrays.copyOf(readContent, (int) files[0].length());
-                else {
-                    if (i != files.length - 1)
-                        target = Arrays.copyOf(readContent, (int) (target.length + files[i + 1].length()));
-                    else
-                        target = Arrays.copyOf(readContent, (int) (target.length + files[i].length()));
-                }
-
+                fos.write(readContent);
+                fos.flush();
+                System.out.println("写入子文件:" + files[i].length() + "文件名:" + files[i].getName());
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        try (fos) {
-            fos.write(target);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        fos.close();
 
     }
 }
